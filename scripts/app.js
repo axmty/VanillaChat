f5edb7290db3bea963603e69e169a3b493b1f8c8
axmty <alexandre.metgy@gmail.com>
2020-04-18 17:08:28 +0200

    Submit message from the form.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,14 @@
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.error(err));
+});
 
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'alex');
