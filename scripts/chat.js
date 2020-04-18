2d5ff0423cfb05fe142dd8207aefbb8cc0680987
axmty <alexandre.metgy@gmail.com>
2020-04-18 14:49:50 +0200

    Create Chatroom for handling data.

--- scripts/chat.js
+++ scripts/chat.js
@@ -2,24 +2,49 @@ function Chatroom(room, username) {
   this.room = room;
   this.username = username;
   this.chats = db.collection('chats');
+  this.roomUnsub;
 }
 
 Chatroom.prototype.addChat = function (message) {
   return new Promise((resolve, reject) => {
     const now = new Date();
-    const chatDocument = {
+    const chat = {
       message,
       username: this.username,
       room: this.room,
       createdAt: firebase.firestore.Timestamp.fromDate(now)
     };
 
-    const response = this.chats.add(chatDocument);
-    resolve(response);
+    const response = this.chats.add(chat).then(result => {
+      resolve(response);
+    }).catch(err => {
+      console.error(err);
+    });
   });
-}
+};
 
-const chatroom = new Chatroom('gaming', 'alex');
-chatroom.addChat('hello everyone')
-  .then(() => console.log('chat added'))
-  .catch(err => console.error(err));
+Chatroom.prototype.sync = function (callback) {
+  this.roomUnsub = this.chats
+    .where('room', '==', this.room)
+    .orderBy('createdAt')
+    .onSnapshot(snap => {
+      snap.docChanges().forEach(change => {
+        if (change.type === 'added') {
+          callback(change.doc.data());
+        }
+      });
+    });
+};
+
+Chatroom.prototype.unsync = function () {
+  this.roomUnsub?.();
+};
+
+Chatroom.prototype.updateName = function (username) {
+  this.username = username;
+};
+
+Chatroom.prototype.updateRoom = function (room) {
+  this.room = room;
+  this.unsync();
+};
