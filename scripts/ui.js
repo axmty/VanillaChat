e00d6e9346b58c5047c80fbff4ccae7556a08e50
axmty <alexandre.metgy@gmail.com>
2020-04-18 17:00:59 +0200

    Beautify message dates.

--- scripts/ui.js
+++ scripts/ui.js
@@ -3,11 +3,15 @@ function ChatUI(list) {
 }
 
 ChatUI.prototype.render = function (data) {
+  const when = dateFns.distanceInWordsToNow(
+    data.createdAt.toDate(),
+    { addSuffix: true }
+  );
   const html = `
     <li class="list-group-item">
       <span class="username">${data.username}</span>
       <span class="message">${data.message}</span>
-      <div class="time">${data.createdAt.toDate()}</div>
+      <div class="time">${when}</div>
     </li>`;
 
   this.list.innerHTML += html;
