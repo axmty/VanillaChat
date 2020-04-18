97b20a3d2b2ba2d074a938c6a08929ece8536552
axmty <alexandre.metgy@gmail.com>
2020-04-18 17:23:11 +0200

    Add localStorage for username and handle update name form.

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,31 @@
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const defaultName = 'anon';
+const defaultRoom = 'general';
 
 newChatForm.addEventListener('submit', e => {
   e.preventDefault();
 
-  const message = newChatForm.message.value.trim();
-  chatroom.addChat(message)
+  const newMessage = newChatForm.message.value.trim();
+  chatroom.addChat(newMessage)
     .then(() => newChatForm.reset())
     .catch(err => console.error(err));
 });
 
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  newNameForm.reset();
+  localStorage.name = newName;
+});
+
+const name = localStorage.name ?? defaultName;
+const room = localStorage.room ?? defaultRoom;
+
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('general', 'alex');
+const chatroom = new Chatroom(room, name);
 
 chatroom.sync(data => chatUI.render(data));
